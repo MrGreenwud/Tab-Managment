b741025866d47b23a533bd11696709892ce5d052
MrGreenwud <108620747+MrGreenwud@users.noreply.github.com>
2023-03-22 15:40:20 +0300

    Init

---
+++ Code/TabView.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TabManagment
+{
+    public class TabView : MonoBehaviour
+    {
+        public virtual void Show() { }
+        public virtual void Hide() { }
+    }
+}
